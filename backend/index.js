0836c6275c1ad157d2a79877a6246d9ae080d7cf
Petri Montonen <91892495+PJM178@users.noreply.github.com>
2023-02-17 16:43:02 +0200

    13.5

--- backend/index.js
+++ backend/index.js
@@ -1,83 +1,20 @@
-require('dotenv').config();
-const { Sequelize, DataTypes, Model } = require('sequelize');
 const express = require('express');
 const app = express();
+
+const { PORT } = require('./util/config');
+const { connectToDatabase } = require('./util/db');
+
+const blogsRouter = require('./controllers/blogs')
+
 app.use(express.json());
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
-});
+app.use('/api/blogs', blogsRouter);
 
-class Blog extends Model {}
+const start = async () => {
+  await connectToDatabase();
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+};
 
-Blog.init({
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-    unique: true,
-  },
-  author: {
-    type: DataTypes.STRING,
-  },
-  url: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  title: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  likes: {
-    type: DataTypes.INTEGER,
-    defaultValue: 0,
-  },
-}, {
-  sequelize,
-  underscored: true,
-  timestamps: false,
-  modelName: 'blog',
-});
-
-Blog.sync();
-
-// HTTP methods
-app.get('/api/blogs', async (req, res) => {
-  const blogs = await Blog.findAll();
-
-  res.json(blogs);
-});
-
-app.post('/api/blogs', async (req, res) => {
-  try {
-    const blog = await Blog.create(req.body);
-
-    return res.json(blog);
-  } catch(error) {
-    return res.status(400).json({ error });
-  }
-});
-
-app.delete('/api/blogs/:id', async (req, res) => {
-  const blog = await Blog.findByPk(req.params.id);
-
-  if (blog) {
-    await blog.destroy();
-
-    return res.status(204).end();
-  } else {
-    return res.status(404).end();
-  }
-});
-
-// Server config
-const PORT = process.env.PORT || 3001;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+start();
