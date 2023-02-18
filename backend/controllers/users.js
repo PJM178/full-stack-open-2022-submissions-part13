bffaa1fefe27b3725a4826a8172cb28045fe780e
Petri Montonen <91892495+PJM178@users.noreply.github.com>
2023-02-18 17:26:03 +0200

    13.8

---
+++ backend/controllers/users.js
@@ -0,0 +1,64 @@
+const router = require('express').Router();
+const bcrypt = require('bcrypt');
+
+const { User, Blog } = require('../models');
+
+router.get('/', async (req, res) => {
+  const users = await User.findAll({
+    include: {
+      model: Blog,
+      attributes: { exlude: ['userId'] },
+    },
+  })
+
+  return res.json(users);
+});
+
+router.post('/', async (req, res, next) => {
+ 
+  // new User should not be used - create or build are preferred
+  // const user = new User({
+  //   username,
+  //   name,
+  //   passwordHash,
+  // });
+
+  // await user.save();
+
+  try {
+    const { username, name, password } = req.body;
+
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(password, saltRounds);
+
+    const newUser = {
+      username: username,
+      name: name,
+      passwordHash: passwordHash,
+    };
+
+    const user = await User.create(newUser);
+
+    return res.json(user);
+  } catch(error) {
+    next(error);
+  }
+});
+
+router.put('/:username', async (req, res, next) => {
+  const user = await User.findOne({ where: { username: req.params.username } })
+  if (user) {
+    try {
+      user.username = req.body.username;
+      await user.save();
+      return res.json(user);
+    } catch(error) {
+      next(error);
+    }
+  } else {
+    return res.status(404).end();
+  }
+  
+});
+
+module.exports = router;
