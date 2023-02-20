22c3aefa25a7130396382ff68cb786dff1b8debd
Petri Montonen <91892495+PJM178@users.noreply.github.com>
2023-02-20 13:37:37 +0200

    13.13.

--- backend/controllers/blogs.js
+++ backend/controllers/blogs.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const jwt = require('jsonwebtoken');
 
 const { SECRET } = require('../util/config');
-
+const { Op } = require('sequelize');
 const { Blog, User } = require('../models');
 
 // Helper functions
@@ -27,12 +27,21 @@ const tokenExtractor = (req, res, next) => {
 
 // HTTP methods
 router.get('/', async (req, res) => {
+  const where = {};
+
+  if (req.query.search) {
+    where.title = {
+      [Op.iLike]: `%${req.query.search}%`,
+    }
+  }
+
   const blogs = await Blog.findAll({
     attributes: { exclude: ['userblogId'] },
     include: {
       model: User,
       attributes: ['name', 'username'],
     },
+    where,
   });
 
   res.json(blogs);
