6e20116e997210abb0eb8a57b9f0c3dcef42090b
Petri Montonen <91892495+PJM178@users.noreply.github.com>
2023-02-20 16:29:30 +0200

    13.16.

---
+++ backend/controllers/authors.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+
+const { Blog } = require('../models');
+const { sequelize } = require('../util/db');
+
+router.get('/', async (req, res) => {
+  const authors = await Blog.findAll({
+    attributes: [
+      'author',
+      [sequelize.fn('COUNT', sequelize.col('title')), 'articles'],
+      [sequelize.fn('SUM', sequelize.col('likes')), 'likes']
+    ],
+    group: ['author'],
+    order: [
+      ['likes', 'DESC']
+    ],
+  });
+  // console.log(JSON.stringify(authors, null, 2));
+  return res.json(authors);
+});
+
+module.exports = router;
