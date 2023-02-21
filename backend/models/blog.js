8f4eabee128e161a7a08a39a31d2123fe6d11b14
Petri Montonen <91892495+PJM178@users.noreply.github.com>
2023-02-21 16:53:25 +0200

    13.18.

--- backend/models/blog.js
+++ backend/models/blog.js
@@ -26,6 +26,15 @@ Blog.init({
     type: DataTypes.INTEGER,
     defaultValue: 0,
   },
+  year: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: new Date().getFullYear(),
+    validate: {
+      min: 1991,
+      max: new Date().getFullYear(),
+    },
+  },
 }, {
   sequelize,
   underscored: true,
