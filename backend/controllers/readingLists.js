afe979e19e8fcfa9583500ca8c4f2bf19d1e624c
Petri Montonen <91892495+PJM178@users.noreply.github.com>
2023-03-05 19:38:12 +0200

    13.22.

--- backend/controllers/readingLists.js
+++ backend/controllers/readingLists.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 const { ReadingList } = require('../models');
+const { tokenExtractor } = require('../util/middleware')
 
 router.post('/', async (req, res, next) => {
   try {
@@ -8,6 +9,25 @@ router.post('/', async (req, res, next) => {
     return res.json(blogAdded);
   } catch(error) {
     next(error);
+  }
+});
+
+router.put('/:id', tokenExtractor, async (req, res, next) => {
+  const blogInList = await ReadingList.findByPk(req.params.id);
+  if (blogInList) {
+    if (blogInList.userId === req.decodedToken.id) {
+      try {
+        blogInList.isRead = req.body.read;
+        await blogInList.save();
+        return res.json(blogInList);
+      } catch(e) {
+        next(e)
+      }
+    } else {
+      return res.status(401).json({ error: 'Invalid user' })
+    }
+  } else {
+    return res.status(404).end();
   }
 });
 
